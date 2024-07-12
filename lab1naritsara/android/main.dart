dabe048d6d1bed22a969fb11288e4b3b39b653a4
NaritsaraWongbutsri <naritsara.w@kkumail.com>
2024-07-12 22:24:46 +0700

    update

--- lab1naritsara/android/main.dart
+++ lab1naritsara/android/main.dart
@@ -15,8 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 251, 0)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 2, 179)),
         useMaterial3: true,
       ),
       home: const MyAppPage(),
@@ -30,40 +29,19 @@ class MyAppPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Student> datas = [
-      Student(
-          "จักรพรรดิ์ อนุไพร",
-          "643450065-4",
-          "image/D.jpg",
-          "About ดิว Birthday 16 JAN 2003",
-          "chakkapad.a@kkumail.com",
-          "https://www.facebook.com/profile.php?id=100011799663716&locale=th_TH"),
-      Student(
-          "นริศรา วงค์บุตรศรี",
-          "643450328-8",
-          "image/narisara.jpg",
-          "About เมย์ Birthday 27 DEC 2002 ",
-          "naritsara.w@kkumail.com.com",
-          "https://www.facebook.com/may.ooy27?locale=th_TH"),
-      Student(
-          "วรรณภา เบ้านาค",
-          "643450330-1",
-          "image/wannapa.jpg",
-          "About นิ่ม Birthday 17 FEB 2003",
-          "wannapa.ba@kkumail.com",
-          "https://www.facebook.com/Wannapa177?locale=th_TH"),
-      Student(
-          "อฆพร ไร่ขาม",
-          "643450334-3",
-          "image/akapron.jpg",
-          "About มอลล่า Birthday 14 FEB 2002",
-          "akapron.r@kkumail.com",
-          "https://www.facebook.com/akaporn.raikham/?locale=th_TH"),
+      Student("จักรพรรดิ์ อนุไพร", "643450065-4", "image/D.jpg", "About ดิว Birthday 16 JAN 2003", "chakkapad.a@kkumail.com", "https://www.facebook.com/profile.php?id=100011799663716&locale=th_TH"),
+      Student("นริศรา วงค์บุตรศรี", "643450328-8", "image/narisara.jpg", "About เมย์ Birthday 27 DEC 2002 ", "naritsara.w@kkumail.com.com", "https://www.facebook.com/may.ooy27?locale=th_TH"),
+      Student("วรรณภา เบ้านาค", "643450330-1", "image/wannapa.jpg", "About นิ่ม Birthday 17 FEB 2003", "wannapa.ba@kkumail.com", "https://www.facebook.com/Wannapa177?locale=th_TH"),
+      Student("อฆพร ไร่ขาม", "643450334-3", "image/akapron.jpg", "About มอลล่า Birthday 14 FEB 2002", "akapron.r@kkumail.com", "https://www.facebook.com/akaporn.raikham/?locale=th_TH"),
+            
+            
+                  
     ];
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Naritsara Wongbutsri App"),
+        title: const Text("Akapron Raikham App"),
       ),
       body: ListView.builder(
           itemCount: datas.length,
